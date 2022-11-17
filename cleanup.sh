87e046061335a04c96cd9a133c4bf679caeacb2a
Johannes Zellner <johannes@nebulon.de>
2022-11-17 18:10:07 +0100

    Also purge preview-cards cache and orphaned media

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,14 @@
 
 set -eu
 
-echo "=> Cleanup cached media"
-
+echo "=> Cleanup"
 cd /app/code
-./bin/tootctl media remove
+
+echo "==> media cache ..."
+./bin/tootctl media remove --days=7
+
+echo "==> orphaned media ..."
+./bin/tootctl media remove-orphans
+
+echo "==> preview cards.."
+./bin/tootctl preview-cards remove --days=7
